588e46d105ca7b63e59a107aba9367d3ab56934d
w3cj <cj@null.computer>
2020-05-31 20:00:02 -0600

    :bug: AHHHH

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { nanoid } = require('nanoid');
 
 require('dotenv').config();
 
-const db = monk(process.env.MONGO_URI);
+const db = monk(process.env.MONGODB_URI);
 const urls = db.get('urls');
 urls.createIndex({ slug: 1 }, { unique: true });
 
