07b12ca0f7df045153b54f510b493742317a417a
w3cj <cj@null.computer>
2020-06-15 12:07:13 -0600

    :sparkles: show rate limit error on the front-end

--- public/app.js
+++ public/app.js
@@ -17,16 +17,19 @@ const app = new Vue({
         },
         body: JSON.stringify({
           url: this.url,
-          slug: this.slug || undefined
-        })
+          slug: this.slug || undefined,
+        }),
       });
-      const result = await response.json();
       if (response.ok) {
+        const result = await response.json();
         this.formVisible = false;
         this.created = `https://cdg.sh/${result.slug}`;
+      } else if (response.status === 429) {
+        this.error = 'You are sending too many requests. Try again in 30 seconds.';
       } else {
+        const result = await response.json();
         this.error = result.message;
       }
-    }
-  }
-})
+    },
+  },
+});
