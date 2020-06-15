20a003cc6cb2389b31af258d5f3a9b209514a8ed
w3cj <cj@null.computer>
2020-06-15 14:26:14 -0600

    :sparkles: remove cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const yup = require('yup');
@@ -19,8 +18,7 @@ const app = express();
 app.enable('trust proxy');
 
 app.use(helmet());
-app.use(morgan('tiny'));
-app.use(cors());
+app.use(morgan('common'));
 app.use(express.json());
 app.use(express.static('./public'));
 
