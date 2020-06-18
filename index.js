0734aa61bbac4209a70f95f1e59b02750aa9ce3c
Bram Verboom <33070319+BramVerb@users.noreply.github.com>
2020-06-18 20:18:54 +0200

    Disallow repeated redirects

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/url', slowDown({
       slug,
       url,
     });
-    if (url.includes('cdg.sh')) {
+    if (url.toLowerCase().includes('cdg.sh')) {
       throw new Error('Stop it. 🛑');
     }
     if (!slug) {
